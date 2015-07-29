ef646ded1513198987e7013f53bf4c5fdd194d32
Imran Ahmed <imran.ahmed@ignitesol.com>
2015-07-29 17:26:06 +0530

    Disable jedi tooltip

--- hooks/python.el
+++ hooks/python.el
@@ -1,6 +1,7 @@
 (add-hook 'python-mode-hook 'jedi:setup)
 (setq jedi:setup-keys t)
 (setq jedi:complete-on-dot t)
+(setq jedi:tooltip-method nil)
 (add-hook 'ein:connect-mode-hook 'ein:jedi-setup)
 
 (defun my/python-mode-hook ()
