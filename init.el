0d60e9080b37bc580701bf602d94a129d1c75c40
Imran Ahmed <imran.ahmed@ignitesol.com>
2015-08-01 10:55:43 +0530

    Always load newest byte code

--- init.el
+++ init.el
@@ -2,14 +2,20 @@
 (require 'cask "~/.cask/cask.el")
 (cask-initialize)
 
+(when (version< emacs-version "24.1")
+  (error "Requires at least GNU Emacs 24.1, but you're running %s" emacs-version))
+
+;; Always load newest byte code
+(setq load-prefer-newer t)
+
 ;; Fix load path
 (when (memq window-system '(mac ns))
-    (exec-path-from-shell-initialize))
+  (exec-path-from-shell-initialize))
 
 ;; Set path
-(setq root-dir "~/.emacs.d")
-(setq core-dir (concat root-dir "/core"))
-(setq hooks-dir (concat root-dir "/hooks"))
+(defvar root-dir (file-name-directory load-file-name))
+(defvar core-dir (expand-file-name "core" root-dir))
+(defvar hooks-dir (expand-file-name "hooks" root-dir))
 
 ;; Load core files
 (add-to-list 'load-path core-dir)
@@ -20,3 +26,5 @@
 
 ;; Load the hooks
 (mapcar 'load (directory-files hooks-dir t "\\.el\\'"))
+
+;;; init.el ends here
