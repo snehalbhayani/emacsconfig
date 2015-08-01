f748aa895373f57a4da46caf9132cc548279b13f
Imran Ahmed <iahmed213@gmail.com>
2015-08-02 01:43:13 +0530

    Underline only errors in buffer with flycheck

--- hooks/flycheck.el
+++ hooks/flycheck.el
@@ -10,3 +10,31 @@
 ;; Disable checkdoc warning
 (with-eval-after-load 'flycheck
   (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc)))
+
+;;; Underline only errors in buffer
+;; Disable underlining provided by flycheck
+(setq flycheck-highlighting-mode nil)
+
+;; Add a custom function for underlining errors
+(require 'flycheck)
+(defun rekenerd-add-lines-for-errors (err)
+  "Credits: https://github.com/iqbalansari"
+  (flycheck-error-with-buffer err
+    (when (equal (flycheck-error-level err) 'error)
+      (pcase-let* ((`(,beg . ,end) (flycheck-error-region-for-mode
+                                    err 'lines))
+                   (overlay (make-overlay beg end))
+                   (level (flycheck-error-level err))
+                   (category (flycheck-error-level-overlay-category level)))
+        (overlay-put overlay 'flycheck-overlay t)
+        (overlay-put overlay 'flycheck-error err)
+        (overlay-put overlay 'category category)
+        (when flycheck-indication-mode
+          (overlay-put overlay 'before-string
+                       (flycheck-error-level-make-fringe-icon
+                        level flycheck-indication-mode)))
+        (overlay-put overlay 'help-echo (flycheck-error-message err))
+        overlay))))
+
+(eval-after-load 'flycheck
+  '(add-to-list 'flycheck-process-error-functions #'rekenerd-add-lines-for-errors))
