57c97a98cba855872a80cc3a5c73da534e2eab87
Imran Ahmed <iahmed213@gmail.com>
2015-08-02 00:32:40 +0530

    Rename function

--- hooks/company.el
+++ hooks/company.el
@@ -3,4 +3,5 @@
                                 (setq company-dabbrev-downcase nil)
                                 (setq company-dabbrev-ignore-case nil)
                                 ))
+
 (add-hook 'after-init-hook 'global-company-mode)
