0f90464b8bc07515ce1a7b53dc9a34e13042b768
Imran Ahmed <imran.ahmed@ignitesol.com>
2015-08-03 09:53:05 +0530

    Add gitignore-mode

--- core/rk-editor.el
+++ core/rk-editor.el
@@ -87,7 +87,7 @@
 ;; which-key
 (which-key-mode +1)
 (which-key-setup-side-window-bottom)
-(setq which-key-idle-delay 0.3)
+;; (setq which-key-idle-delay 0.3)
 (setq which-key-side-window-max-height 0.5)
 
 ;; Scratch buffer configuration
