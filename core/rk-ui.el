d35a77f9e25e0426dc5da62acac2634a332f8d95
Imran Ahmed <imran.ahmed@ignitesol.com>
2015-08-17 14:54:32 +0530

    Show timestamp in Messages buffer

--- core/rk-ui.el
+++ core/rk-ui.el
@@ -45,4 +45,21 @@
 ;; Replace yes-no questions with y-n questions
 (fset 'yes-or-no-p 'y-or-n-p)
 
+;;; Timestamps in *Messages*
+;; via http://www.reddit.com/r/emacs/comments/1auqgm/speeding_up_your_emacs_startup/
+(defun current-time-microseconds ()
+  (let* ((nowtime (current-time))
+         (now-ms (nth 2 nowtime)))
+    (concat (format-time-string "[%Y-%m-%dT%T" nowtime) (format ".%d] " now-ms))))
+
+(defadvice message (before test-symbol activate)
+  (if (not (string-equal (ad-get-arg 0) "%s%s"))
+      (let ((inhibit-read-only t)
+            (deactivate-mark nil))
+        (with-current-buffer "*Messages*"
+          (goto-char (point-max))
+          (if (not (bolp))
+              (newline))
+          (insert (current-time-microseconds))))))
+
 (provide 'rk-ui)
