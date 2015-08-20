838dff102d445a3d7d53d9eb34f4675dea1a5ba1
Imran Ahmed <imran.ahmed@ignitesol.com>
2015-08-20 14:56:50 +0530

    Disble whitespace mode for yaml

--- hooks/yaml.el
+++ hooks/yaml.el
@@ -3,5 +3,5 @@
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
 (add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
 
-(add-hook 'yaml-mode-hook 'whitespace-mode)
+;; (add-hook 'yaml-mode-hook 'whitespace-mode)
 (add-hook 'yaml-mode-hook 'subword-mode)
