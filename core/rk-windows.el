4995f54db5887691ecdb518bc9c01a0e62540dfd
Imran Ahmed <iahmed213@gmail.com>
2015-08-25 23:12:29 +0530

    Add function to kill other buffers

--- core/rk-windows.el
+++ core/rk-windows.el
@@ -13,4 +13,16 @@
 (global-set-key (kbd "C-x -") #'rekenerd-split-window-vertically)
 (global-set-key (kbd "C-x \\") #'rekenerd-split-window-horizontally)
 
+(defun rekenerd-kill-other-buffers ()
+  "Kill all buffers but the current one"
+  (interactive)
+  (when (y-or-n-p "Are you sure you want to kill all buffers but the current one?")
+    (-each
+        (->> (buffer-list)
+             (-filter #'buffer-file-name)
+             (--remove (eql (current-buffer) it)))
+      #'kill-buffer)))
+
+(global-set-key (kbd "C-c k") 'rekenerd-kill-other-buffers)
+
 (provide 'rk-windows)
--- hooks/programming.el
+++ hooks/programming.el
@@ -1,5 +1,7 @@
 (defun rekenerd-font-lock-comment-annotations ()
-  "credit: https://github.com/bbatsov"
+  "credit: https://github.com/bbatsov
+   Highlight a bunch of well known comment annotations.
+   This functions should be added to the hooks of major modes for programming."
   (font-lock-add-keywords
    nil '(("\\<\\(\\(FIX\\(ME\\)?\\|TODO\\|OPTIMIZE\\|HACK\\|REFACTOR\\):\\)"
           1 font-lock-warning-face t))))
