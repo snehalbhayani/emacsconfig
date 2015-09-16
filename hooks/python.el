b122fcf02ec86d0b90a69239de8e3af2064a3905
Imran Ahmed <imran.ahmed@ignitesol.com>
2015-09-16 16:37:34 +0530

    Setup jedi before config

--- hooks/python.el
+++ hooks/python.el
@@ -3,7 +3,6 @@
 (add-hook 'python-mode-hook 'py-autopep8-enable-on-save)
 
 (require 'jedi)
+(add-hook 'python-mode-hook 'jedi:setup)
 (setq jedi:complete-on-dot t)
 (setq jedi:tooltip-method nil)
-(add-hook 'ein:connect-mode-hook 'ein:jedi-setup)
-(add-hook 'python-mode-hook 'jedi:setup)
