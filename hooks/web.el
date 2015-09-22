d4fd189039fbd75c88c62548b7b11d603b95769e
Imran Ahmed <imran.ahmed@ignitesol.com>
2015-09-22 14:52:28 +0530

    Use 'css-mode' for CSS

--- hooks/web.el
+++ hooks/web.el
@@ -1,15 +1,12 @@
 (require 'web-mode)
 
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.css\\'" . css-mode))
 
 (defun rk/web-mode-hook ()
-  (setq web-mode-markup-indent-offset 2)
-  (setq web-mode-css-indent-offset 2)
-  (setq web-mode-code-indent-offset 2)
-  (setq web-mode-enable-auto-pairing t)
-  (setq web-mode-enable-css-colorization t))
+  (setq web-mode-markup-indent-offset 4)
+  (setq web-mode-code-indent-offset 4)
+  (setq web-mode-enable-auto-pairing t))
 
 (add-hook 'web-mode-hook 'rk/web-mode-hook)
 
