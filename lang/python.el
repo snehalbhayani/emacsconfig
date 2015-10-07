9055f42bb222c592004de5c19a1b16ba713ba2f7
Imran Ahmed <imran.ahmed@ignitesol.com>
2015-10-07 10:25:43 +0530

    Disble eldoc mode for python

--- lang/python.el
+++ lang/python.el
@@ -1,3 +1,7 @@
+(add-hook 'after-init-hook
+          #'(lambda ()
+              (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))))
+
 (require 'py-autopep8)
 (setq py-autopep8-options '("--max-line-length=149"))
 (add-hook 'python-mode-hook 'py-autopep8-enable-on-save)
@@ -6,3 +10,6 @@
 (add-hook 'python-mode-hook 'jedi:setup)
 (setq jedi:complete-on-dot t)
 (setq jedi:tooltip-method nil)
+
+;; Disble eldoc
+(add-hook 'python-mode-hook (lambda () (eldoc-mode -1)))
