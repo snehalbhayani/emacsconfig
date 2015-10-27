2ce8fa79a4273c6972f1669aab0e37e17d9acd8a
Imran Ahmed <imran.ahmed@ignitesol.com>
2015-10-27 10:31:28 +0530

    Minor change

--- hooks/programming.el
+++ hooks/programming.el
@@ -3,7 +3,7 @@
    Highlight a bunch of well known comment annotations.
    This functions should be added to the hooks of major modes for programming."
   (font-lock-add-keywords
-   nil '(("\\<\\(\\(FIX\\(ME\\)?\\|TODO\\|OPTIMIZE\\|HACK\\|REFACTOR\\):\\)"
+   nil '(("\\<\\(\\(FIX\\(ME\\)?\\|TODO\\|OPTIMIZE\\|HACK\\|REFACTOR\\)\\)"
           1 font-lock-warning-face t))))
 
 (defun rk/prog-mode-defaults ()
