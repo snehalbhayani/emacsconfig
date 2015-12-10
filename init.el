4096d55f6a11b9ee462dad20affe626d0c8c51ec
Imran Ahmed <imran.ahmed@ignitesol.com>
2015-12-10 10:59:17 +0530

    Remove unnecessary settings

--- init.el
+++ init.el
@@ -55,19 +55,3 @@
 
 (mapc 'load (directory-files hooks-dir t "\\.el\\'"))
 (mapc 'load (directory-files lang-dir t "\\.el\\'"))
-(put 'downcase-region 'disabled nil)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(blink-cursor-mode nil)
- '(column-number-mode t)
- '(show-paren-mode t)
- '(tool-bar-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
